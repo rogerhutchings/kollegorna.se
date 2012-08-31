951599ce35357479c718795a4ddd13305ee4a73a
Per Sandström <per@helloper.com>
2012-08-31 20:35:40 +0200

    Added Mapbox map

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 MBP.scaleFix();
 MBP.hideUrlBar();
+
+mapbox.auto('footer-map', 'kollegorna.map-rbr6h31m');
+
+$(function() {
+  $('.map-link').click(function() {
+    $('.footer-map').slideToggle('fast', function(){
+      $('html, body').animate({scrollTop: $(document).height()}, 'slow');
+    });
+    return false;
+  });
+});
