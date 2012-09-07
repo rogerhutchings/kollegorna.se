dc0d18f3f614baf0de58035b0e0dd71a0a5ab2f9
Per Sandström <per@helloper.com>
2012-09-07 15:12:03 +0200

    Content updates

--- js/main.js
+++ js/main.js
@@ -1,15 +1,6 @@
 MBP.scaleFix();
 MBP.hideUrlBar();
 
-mapbox.auto('footer-map', 'kollegorna.map-rbr6h31m');
-
 $(function() {
   $(".logo, .header-logo").lettering();
-
-  $('.map-link').click(function() {
-    $('.footer-map').slideToggle('fast', function(){
-      $('html, body').animate({scrollTop: $(document).height()}, 'slow');
-    });
-    return false;
-  });
 });
