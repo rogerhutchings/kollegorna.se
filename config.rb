b5f2609b4e8d5500ddfd73e4d0372dc7dc530599
Per Sandström <per@helloper.com>
2013-06-14 15:11:28 +0200

    Lots of stuff…

---
+++ config.rb
@@ -0,0 +1,12 @@
+require 'breakpoint'
+
+#
+# Default Compass settings
+#
+
+http_path = "/"
+css_dir = "stylesheets/css"
+sass_dir = "stylesheets/sass"
+relative_assets = true
+line_comments = true
+output_style = :expanded
--- js/main.js
+++ js/main.js
@@ -5,4 +5,36 @@ $(function() {
   $(".logo, .header-logo").lettering();
 
   $('.post').fitVids();
+
+
+  $(".navigation-toggle").hover(
+    function () {
+      $('.navigation').addClass('navigation-expanded');
+      $('.navigation').removeClass('navigation-box-shadow');
+      $(this).addClass('navigation-toggle-active');
+      $('.navigation-contents').slideDown('fast', function() {
+
+      });
+      $('.page-overlay').fadeIn('fast');
+    },
+    function () {
+
+    }
+  );
+
+  $(".navigation").hover(
+    function () {
+
+    },
+    function () {
+      $(this).removeClass('navigation-expanded');
+      $('.navigation-contents').slideUp('fast', function() {
+        $('.navigation-toggle').removeClass('navigation-toggle-active');
+        $('.navigation').addClass('navigation-box-shadow');
+        //$(".navigation-toggle").fadeIn('medium');
+        $('.page-overlay').fadeOut('fast');
+      });
+    }
+  );
+
 });
