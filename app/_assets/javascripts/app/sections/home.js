3de2cdcb116c0c852c4df2f7808d5e39e300d1cd
Per Sandström <per@helloper.com>
2014-03-20 17:44:58 +0100

    Playing with header and menu

--- app/_assets/javascripts/app/sections/home.js
+++ app/_assets/javascripts/app/sections/home.js
@@ -1,6 +1,31 @@
 $(function() {
-  /*$('.selected-projects').flexslider({
-    animation: 'slide',
-    directionNav: false
-  });*/
+  $('.site-intro').height($(window).height());
+});
+
+//var menu_target = $(".layout__up-to-two-columns").offset().top;
+var menu_target = $(window).height() / 3;
+var scroll_previous = 0;
+
+$(document).scroll(function(){
+  if($(this).scrollTop() > menu_target) {
+    $('.header-background').fadeIn('fast');
+    $('.header').addClass('header--background');
+  } else {
+    $('.header-background').fadeOut('fast');
+    $('.header').removeClass('header--background');
+  }
+
+  var scroll_current = $(this).scrollTop();
+  if (scroll_current < scroll_previous){
+    if ($('.header').hasClass("header--hidden")) {
+      $('.header').removeClass('header--hidden');
+      $('.header').fadeIn('fast');
+    }
+  } else {
+    if (scroll_current > 0) {
+      $('.header').addClass('header--hidden');
+      $('.header').fadeOut('fast');
+    }
+  }
+  scroll_previous = scroll_current;
 });
