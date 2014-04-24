f8bc6ea5aac8d90e911bd0145ca95f75e85c288e
Per Sandström <per@helloper.com>
2014-04-24 14:10:09 +0200

    Blog header

--- app/_assets/javascripts/app/main.js
+++ app/_assets/javascripts/app/main.js
@@ -1,3 +1,14 @@
 $(function() {
   $(".logo, .header-logo").lettering();
+
+  // Typer
+  // http://cosmos.layervault.com/typer-js.html
+  $('[data-typer-targets]').typer()
+
+  // SVG fallback
+  if (!Modernizr.svg) {
+    $("img[src$='.svg']").each(function() {
+      $(this).attr("src", $(this).data('fallback'));
+    });
+  }
 });
