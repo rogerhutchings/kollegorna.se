65c001812f2b26423de8d0f7bd0fbe0fbb09f08e
Per Sandström <per@helloper.com>
2014-10-08 11:40:50 +0200

    jQuery.typer bower config is broken so just add the file

--- app/_assets/javascripts/app.js
+++ app/_assets/javascripts/app.js
@@ -2,7 +2,7 @@
 //= require bower_components/fitvids/jquery.fitvids.js
 //= require bower_components/Lettering.js/jquery.lettering.js
 //= require bower_components/jquery.lazyload/jquery.lazyload.min.js
-//= require jquery.typer/jquery.typer.js
+//= require app/jquery.typer.js
 
 //= require app/main.js
 //= require app/navigation.js
