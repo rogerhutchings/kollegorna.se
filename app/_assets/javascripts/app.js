e6576237c111afc1f78326b213f77aee0a92e3e1
Dennis Carlsson <dencarlsson@gmail.com>
2015-03-02 16:53:36 +0100

    Updated colleagues.yml, humans.txt and fixed app.js

--- app/_assets/javascripts/app.js
+++ app/_assets/javascripts/app.js
@@ -1,7 +1,7 @@
 //= require bower_components/jquery/dist/jquery.min.js
 //= require bower_components/fitvids/jquery.fitvids.js
 //= require bower_components/Lettering.js/jquery.lettering.js
-//= require bower_components/jquery.lazyload/jquery.lazyload.min.js
+//= require bower_components/jquery.lazyload/jquery.lazyload.js
 //= require app/jquery.typer.js
 
 //= require app/main.js
