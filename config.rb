2801cf000797f5d8afd32553f60cf1d747d2f24d
Per Sandström <per@helloper.com>
2015-06-15 15:24:25 +0200

    Add favicons

--- config.rb
+++ config.rb
@@ -24,9 +24,8 @@ end
 activate :directory_indexes
 
 # Minimize css/js and fix assets for Build
-configure :build do 
+configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :relative_assets
   activate :asset_hash
 end
